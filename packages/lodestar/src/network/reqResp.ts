644d1c01c198caa53c1b8326887b104f22e63a0c
Tuyen Nguyen <vutuyen2636@gmail.com>
2020-04-20 10:54:31 +0700

    Refactor req/resp to better handle different method types

--- packages/lodestar/src/network/reqResp.ts
+++ packages/lodestar/src/network/reqResp.ts
@@ -16,7 +16,8 @@ import {
   Metadata,
 } from "@chainsafe/lodestar-types";
 import {IBeaconConfig} from "@chainsafe/lodestar-config";
-import {Method, ReqRespEncoding, RequestId, RESP_TIMEOUT, RpcErrorCode, TTFB_TIMEOUT,} from "../constants";
+import {Method, ReqRespEncoding, RequestId, RESP_TIMEOUT, RpcErrorCode, TTFB_TIMEOUT,
+  SINGLE_CHUNK_METHODS, NO_CHUNK_METHODS,} from "../constants";
 import {ILogger} from "@chainsafe/lodestar-utils/lib/logger";
 import {createResponseEvent, createRpcProtocol, randomRequestId} from "./util";
 import {IReqResp, ReqEventEmitter, RespEventEmitter, ResponseCallbackFn, ResponseChunk} from "./interface";
@@ -129,7 +130,7 @@ export class ReqResp extends (EventEmitter as IReqEventEmitterClass) implements 
     return await this.sendRequest<Status>(peerInfo, Method.Status, request);
   }
   public async goodbye(peerInfo: PeerInfo, request: Goodbye): Promise<void> {
-    await this.sendRequest<Goodbye>(peerInfo, Method.Goodbye, request, true);
+    await this.sendRequest<Goodbye>(peerInfo, Method.Goodbye, request);
   }
   public async ping(peerInfo: PeerInfo, request: Ping): Promise<Ping> {
     return await this.sendRequest<Ping>(peerInfo, Method.Ping, request);
@@ -156,19 +157,19 @@ export class ReqResp extends (EventEmitter as IReqEventEmitterClass) implements 
     const getResponse = this.getResponse;
     return (source: AsyncIterable<Buffer|BufferList>) => {
       return (async function * () {
-        if (method === Method.Metadata) {
-          yield* getResponse(peerId, method, undefined);
-        }
+        let data: Buffer;
+        // source is an array of 1 item or empty array
         for await (const val of source) {
-          const data = Buffer.isBuffer(val) ? val : val.slice();
-          yield* getResponse(peerId, method, data);
+          data = Buffer.isBuffer(val) ? val : val.slice();
+          break;
         }
+        yield* getResponse(peerId, method, data);
       })();
     };
   }
 
   private getResponse = (peerId: PeerId, method: Method, data: Buffer): AsyncIterable<ResponseChunk> => {
-    const request = method === Method.Metadata ? undefined : this.encoder.decodeRequest(method, data);
+    const request = data ? this.encoder.decodeRequest(method, data) : undefined;
     const requestId = randomRequestId();
     this.logger.verbose(`${requestId} - receive ${method} request from ${peerId.toB58String()}`);
     // eslint-disable-next-line
@@ -181,7 +182,7 @@ export class ReqResp extends (EventEmitter as IReqEventEmitterClass) implements 
       responseTimer = this.responseListener.waitForResponse(requestId, responseListenerFn);
       this.emit("request", new PeerInfo(peerId), method, requestId, request);
     });
-    
+
     return (async function * () {
       const responseIter = await promise;
       yield* responseIter;
@@ -192,8 +193,9 @@ export class ReqResp extends (EventEmitter as IReqEventEmitterClass) implements 
     peerInfo: PeerInfo,
     method: Method,
     body?: RequestBody,
-    requestOnly?: boolean
   ): Promise<T> {
+    const requestOnly = NO_CHUNK_METHODS.includes(method);
+    const requestSingleChunk = SINGLE_CHUNK_METHODS.includes(method);
     return await new Promise((resolve, reject) => {
       this.logger.verbose(`send ${method} request to ${peerInfo.id.toB58String()}`);
       let responseTimer = setTimeout(() => reject(new RpcError(RpcErrorCode.ERR_RESP_TIMEOUT)), TTFB_TIMEOUT);
@@ -213,13 +215,12 @@ export class ReqResp extends (EventEmitter as IReqEventEmitterClass) implements 
               responses.push(response);
             }
             cancelTimer();
-            if (!requestOnly && method === Method.Status && responses.length === 0) {
+            if (requestSingleChunk && responses.length === 0) {
               // allow empty response for beacon blocks by range/root
               reject(`No response returned for method ${method}`);
               return;
             }
-            const finalResponse =
-              [Method.Status, Method.Ping, Method.Metadata].includes(method) ? responses[0] : responses;
+            const finalResponse = requestSingleChunk ? responses[0] : responses;
             this.logger.verbose(`receive ${method} response from ${peerInfo.id.toB58String()}`);
             resolve(requestOnly? undefined : finalResponse as T);
           });
@@ -235,7 +236,7 @@ export class ReqResp extends (EventEmitter as IReqEventEmitterClass) implements 
     body?: RequestBody,
   ): AsyncIterable<T> {
     const {encoder, libp2p, logger} = this;
-    
+
     return (async function * () {
       const protocol = createRpcProtocol(method, encoder.encoding);
       const {stream} = await libp2p.dialProtocol(peerInfo, protocol) as {stream: Stream};
